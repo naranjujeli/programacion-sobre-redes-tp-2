9f7f2fea2de43fdea3585e51a89258424054e3c2
naranjujeli <lucadive@gmail.com>
2023-05-20 19:07:52 -0300

    Agregar el archivo stdafx.h como header pre-compilado

---
+++ stdafx.h
@@ -0,0 +1,16 @@
+#ifndef STDAFX_H
+#define STDAFX_H
+
+#include <iostream>
+#include <string>
+#include <stdlib.h>
+#include <math.h>
+#include <vector>
+#include <thread>
+#include <atomic>
+#include <exception>
+#include <ssteam>
+#include <fstream>
+#include <unistd.h>
+
+#endif
